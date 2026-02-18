a8f4aeb59c67575d2df8b61e7957dba3cfeeb3e7
River Jiang <river@users.noreply.github.com>
2026-02-17 19:50:39 -0800

    Fix LaunchBar action: quote binary path to handle spaces

--- LaunchBar Action/Join Next Meeting.lbaction/Contents/Scripts/default.js
+++ LaunchBar Action/Join Next Meeting.lbaction/Contents/Scripts/default.js
@@ -1,5 +1,5 @@
 function run() {
     LaunchBar.hide();
     var binary = Action.path + '/Contents/Resources/join-next-meeting';
-    LaunchBar.execute('/bin/sh', '-c', binary + ' 2>/dev/null &');
+    LaunchBar.execute('/bin/sh', '-c', '"' + binary + '" 2>/dev/null &');
 }
